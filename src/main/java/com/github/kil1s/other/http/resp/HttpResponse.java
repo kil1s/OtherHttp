e3a1e9cd41d2a9b10b5bbac9aad8b76b499ad34f
Florian L. Steenbuck <admin@kil.ls>
2025-03-24 21:38:57 +0100

    <JAVA> / Response / (use) body logic, (add) unknown headers

--- src/main/java/com/github/kil1s/other/http/resp/HttpResponse.java
+++ src/main/java/com/github/kil1s/other/http/resp/HttpResponse.java
@@ -1,8 +1,10 @@
 package com.github.kil1s.other.http.resp;
 
+import com.github.kil1s.other.http.resp.headers.map.HttpHeaders;
 import com.github.kil1s.other.http.resp.status.HttpResponseStatus;
 
-public interface HttpResponse {
+public interface HttpResponse<H extends HttpHeaders<?, ?, ?, ?, ?>> {
     HttpResponseStatus getStatus();
-
+    HttpBody getBody();
+    H getHeaders();
 }
